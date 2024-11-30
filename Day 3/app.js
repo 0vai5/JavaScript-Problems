9397841180d4cd4623a7e0e713b6ebe11bf5c00f
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-11-30 18:21:14 +0500

    Day 3 completed

---
+++ Day 3/app.js
@@ -0,0 +1,37 @@
+// sum of cubes
+
+// function sumOfCubes(arr) {
+//   let total = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     total += arr[i] ** 3
+//   }
+
+//   return total;
+// }
+
+// console.log(sumOfCubes([1, 5, 9])); // 855
+// console.log(sumOfCubes([2])); // 8
+// console.log(sumOfCubes([])); // 0
+// console.log(sumOfCubes([1, 2, 3, 4, 5])); // 225
+
+// Factorial
+
+// function factorial(num) {
+//   if (num === 0) return 1;
+//   let total = 1;
+//   let index = 1;
+
+//   do {
+//     total *= index;
+//     index++;
+//   } while (index <= num);
+
+//   return total;
+// }
+
+// console.log(factorial(0)); // 1
+// console.log(factorial(1)); // 1
+// console.log(factorial(2)); // 2
+// console.log(factorial(3)); // 6
+
+
