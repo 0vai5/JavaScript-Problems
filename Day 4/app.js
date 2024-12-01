51143a7e84addfb5f37e442f9c3984a0a5afa919
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-12-01 18:37:33 +0500

    Day 4 completed

---
+++ Day 4/app.js
@@ -0,0 +1,48 @@
+// First letter of each letter in a sentence is capitalized
+
+// function titleCase(sentence) {
+
+//     let words = sentence.toLowerCase().split(" ");
+//     let titleCased = words.map((word) => {
+//         return word[0].toUpperCase() + word.slice(1)
+//     })
+
+//     return titleCased.join(" ")
+
+// }
+
+// console.log(titleCase("hello how are you")) // "Hello How Are You"
+// console.log(titleCase("hello")) // "Hello"
+
+// Reverse a string
+
+//  function reverseWords(sentence) {
+
+//     var words = sentence.split(" ");
+
+//     var reversed = words.reverse()
+
+//     return reversed.join(" ")
+
+// }
+
+// console.log(reverseWords("Hello World"))
+// console.log(reverseWords("A Santa at NASA"))
+
+
+// Maximum Number
+
+// function findMax(arr) {
+//     var max = arr[0];
+
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
+
+//     return max
+// }
+
+// console.log(findMax([3, 1, 4, 1, 5, 9, 2, 6])) // 9
+// console.log(findMax([5, 5, 5])) // 5
