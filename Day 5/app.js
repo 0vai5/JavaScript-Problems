c09bf1241a81edead607dbcfdff5a349d5bdcde0
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-12-02 19:25:49 +0500

    Day 5 completed

---
+++ Day 5/app.js
@@ -0,0 +1,36 @@
+// Prime Number
+
+// function isPrime(num) {
+//     if(num <= 1) return false;
+//     for(let i = 2; i < num; i++) {
+//         if(num % i === 0) return false;
+//     }
+//     return true;
+// }
+
+// console.log(isPrime(7)) // true
+// console.log(isPrime(97)) // true
+// console.log(isPrime(43)) // true
+// console.log(isPrime(9)) // false
+
+// Merge Sorted Array
+
+// function mergeSortedArrays(arr1, arr2) {
+//     let merged = [...arr1, ...arr2];
+//     return merged.sort((a, b) => a - b)
+// }
+
+// console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])) // [1, 2, 3, 4, 5, 6]
+// console.log(mergeSortedArrays([1, 2, 3], [4, 5, 6])) // [1, 2, 3, 4, 5, 6]
+
+// Calculate Power
+
+// function power(base, exponent) {
+//     // write your code below
+//     return Math.pow(base, exponent)
+// }
+
+// console.log(power(2, 3)) // 8
+// console.log(power(3, 3)) // 27
+
+
