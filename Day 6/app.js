12737ebc5467f692891ffbb4aa08025b772932d1
Muhammad Ovais Raza <129333571+0vai5@users.noreply.github.com>
2024-12-03 19:14:20 +0500

    Day 6 completed

---
+++ Day 6/app.js
@@ -0,0 +1,24 @@
+// Reverse Integer
+
+// function reverseInteger(num) {
+//     let strNum = num.toString();
+//     let reversed = strNum.split("").reverse().join("");
+//     return parseInt(reversed) * Math.sign(num)
+// }
+
+// console.log(reverseInteger(123)) // 321
+// console.log(reverseInteger(-123)) // -321
+
+
+// Count Words
+
+// function countWords(sentence) {
+//   let formattedStr = sentence.replace(/[^a-zA-Z ]/g, "").toLowerCase();
+
+//   let splitted = formattedStr.split(" ");
+//   return splitted.length;
+// }
+
+// console.log(countWords("Hello how are you?")); // 4
+
+
